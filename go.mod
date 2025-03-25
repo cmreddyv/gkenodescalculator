61290221837b6627c095f859cc9a518abc67c2f0
cmreddyv <cmreddy.v@gmail.com>
2025-03-25 21:52:46 +1100

    adding inital code of nodepool calc

---
+++ go.mod
@@ -0,0 +1,3 @@
+module miniprojectgo
+
+go 1.22.2
---
+++ main.go
@@ -0,0 +1,41 @@
+//get maximum number of pods per node
+
+//number of pods allocated to every node = 2 * maximum pods per node = 2 * 32 = 64 = 2^6
+
+//number of ips in the cluster pod range = 10.96.2.0/23 = 2 ^(32- pod cir range) = 2 ^ 9
+
+// maximum number od node = 2 ^ 9 divided by 2 ^ 6 = 2 ^ 3 = 8 nodes
+
+package main
+
+import (
+	"fmt"
+	"math"
+)
+
+func main() {
+
+	var i, j int
+
+	fmt.Print("enter the max number of pods in node pool: ")
+
+	fmt.Scan(&i)
+
+	fmt.Print("enter the subnet range of the pods: 32, 24, 22, 16: ")
+
+	fmt.Scan(&j)
+
+	fmt.Println("j is:", j)
+
+	if j < 8 || j > 32 {
+
+		fmt.Println("enter the range of pod subnet between 8 and 32 only")
+
+	} else {
+
+		k := float64((32 - j))
+
+		fmt.Println("maximum number of node in a cluster: ", math.Pow(2, k)/float64(2*i))
+	}
+
+}
